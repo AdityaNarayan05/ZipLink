3a9870f9901011c7a284c7248a3ad16dd7abee09
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-03-27 21:58:13 +0530

    Configuring diff ejs file in index.js

--- index.js
+++ index.js
@@ -58,6 +58,224 @@ var already = false;
 
 
 
+
+
+
+//Get request for "/" route
+app.get("/", function (req, res) {
+    already = false;
+    hash = '';
+    isAlias = false;
+    res.render("home");
+});
+
+//Get request for "/shorten" route
+app.get("/shorten", function (req, res) {
+    //Check if the user is authenticated
+    if (req.isAuthenticated()) {
+        if (already) {
+            res.render("shorten", {
+                slink: "Alias already taken!",
+            });
+        }
+        else if (!isAlias) {
+            res.render("shorten", {
+                slink: req.hostname + "/go/" + hashValue,
+            });
+        }
+        else {
+            res.render("shorten", {
+                slink: req.hostname + "/i/" + hashValue,
+            });
+        }
+
+    }
+    else {
+        //Render "login" page if user is not logged-in.
+        res.redirect("/login");
+    }
+});
+
+//Get request for "/login" route
+app.get("/login", function (req, res) {
+    //Check if user is authenticated
+    if (req.isAuthenticated()) {
+        //Redirect to "/shorten" page if user is logged-in
+        res.redirect("/shorten");
+    }
+    else {
+        //Render "login" page if user is not logged-in.
+        res.render("login");
+    }
+});
+
+//Get request for "/register" route
+app.get("/register", function (req, res) {
+    //Check if user is authenticated
+    if (req.isAuthenticated()) {
+        //Redirect to "/shorten" page if user is logged-in
+        res.redirect("/shorten");
+    }
+    else {
+        //Render "register" page if user is not logged-in.
+        res.render("register");
+    }
+});
+
+app.get("/go/:slink", async function (req, res) {
+
+    const slink = req.params.slink;
+    const id = ShortURL.decode(slink);
+    const doc = await Link.findById(id);
+    const update = { clicks: 1 + doc.clicks };
+    const up = await doc.updateOne(update);
+    res.redirect(307, doc.link);
+});
+
+app.get("/i/:slink", async function (req, res) {
+
+    const slink = req.params.slink;
+    var doc = await Link.find({ hash: slink });
+    //console.log(doc);
+    await Link.updateOne({ hash: slink }, {
+        clicks: doc[0].clicks + 1
+    });
+    res.redirect(307, doc[0].link);
+});
+
+//Get request for "/logout" route
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/");
+        }
+    });
+});
+
+//Get reqest for "/contact" route
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+
+//Post request for "/register" route
+app.post("/register", function (req, res) {
+    const name = req.body.name;
+    //Register a user into database
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            //Saving session cookies after successful registration
+            passport.authenticate("local")(req, res, function () {
+                //Find and update name of the user into database (Naming is required for some options).
+                User.findOneAndUpdate({ _id: req.user.id }, { $set: { name: name } }, function (err) {
+                    if (!err) {
+                        //Redirect to "/shorten" page after successful registration
+                        res.redirect("/shorten");
+                    }
+                });
+            });
+        }
+    });
+});
+
+//Post request for "/login" route
+app.post("/login", async function (req, res) {
+    //Create user object from the login form data
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    const userExist = await User.find({ username: req.body.username });
+    if (userExist.length <= 0) {
+        res.redirect("/register");
+    }
+    else {
+        req.login(user, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                //If user is found in database, save the login session and redirect to "/shorten" page
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/shorten");
+                });
+            }
+        });
+    }
+
+});
+
+app.post("/shorten", async function (req, res) {
+    if (req.isAuthenticated()) {
+        var link = req.body.link;
+        var ind = link.search("://");
+        if (ind === -1) {
+            link = "http://" + link;
+        }
+        const alias = req.body.alias;
+        if (alias === "") {
+            already = false;
+            isAlias = false;
+            var id = await Next.find({});
+            id = id[0].next;
+            Next.updateOne({ _id: 1 }, { $set: { 'next': id + 1 } }, async function (err, results) {
+                if (!err) {
+                    const hash = ShortURL.encode(id);
+                    console.log(hash);
+                    const newLink = new Link({
+                        _id: id,
+                        link: link,
+                        isCustom: false,
+                        hash: hash,
+                        clicks: 1,
+                    });
+                    await newLink.save();
+                    hashValue = hash;
+                    res.redirect("/shorten");
+                }
+            });
+        }
+        else {
+            var id = await Next.find({});
+            id = id[0].next;
+            Next.updateOne({ _id: 1 }, { $set: { 'next': id + 1 } }, async function (err, results) {
+                if (!err) {
+                    const hash = alias;
+                    console.log(hash);
+                    const doc = await Link.find({ hash: hash });
+                    if (doc.length <= 0) {
+                        already = false;
+                        const newLink = new Link({
+                            _id: id,
+                            link: link,
+                            isCustom: true,
+                            hash: hash,
+                            clicks: 1,
+                        });
+                        await newLink.save();
+                        hashValue = hash;
+                        isAlias = true;
+                        res.redirect("/shorten");
+                    }
+                    else {
+                        already = true;
+                        res.redirect("/shorten");
+                        isAlias = true;
+                    }
+
+                }
+            });
+        }
+    }
+});
+
+
 //3000 for localhost (127.0.0.1) and dynamic port for Heroku and other Node.JS services
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, function () {
