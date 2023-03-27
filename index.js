e2d094dced54c244d73a2bb7247d11140ccec351
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-03-27 13:57:58 +0530

    Created Ejs Template for Different Page

--- index.js
+++ index.js
@@ -17,7 +17,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const server = http.createServer(app); 
 
+//ShortURL Logic
+var ShortURL = new function () {
+    var _alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_',
+        _base = _alphabet.length;
 
+    this.encode = function (num) {
+        var str = '';
+        while (num > 0) {
+            str = _alphabet.charAt(num % _base) + str;
+            num = Math.floor(num / _base);
+        }
+        return str;
+    };
+
+    this.decode = function (str) {
+        var num = 0;
+        for (var i = 0; i < str.length; i++) {
+            num = num * _base + _alphabet.indexOf(str.charAt(i));
+        }
+        return num;
+    };
+};
+
+//Stores the generated Hash Value
+var hashValue = "";
+
+//Stores whether the Hash is Custom Alias or not.
+var isAlias = false;
+
+//Stores whether the Hash is already taken or not.
+var already = false;
 
 
 
