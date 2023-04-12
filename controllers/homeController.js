258b1c46ecbbb7323d127b29aae507d02201cc01
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-04-12 16:02:57 +0530

    Modified the workflow of Project

---
+++ controllers/homeController.js
@@ -0,0 +1,11 @@
+
+exports.getHome = function (req, res) {
+    already = false;
+    hash = '';
+    isAlias = false;
+    res.render("home");
+}
+
+exports.getContactPage = function (req, res) {
+    res.render("contact");
+}
